051475962a248e13fffab4c93bde500dfbcb2291
sydyk120276 <akbar@github.com>
2023-01-28 16:11:58 +0600

    added validation for Cyrillic

--- js/script.js
+++ js/script.js
@@ -89,6 +89,11 @@ let validation = new JustValidate('#form')
 
 validation.addField("#email", [
   {
+    rule: 'customRegexp',
+    value: /^[A-Za-z0-9][A-Za-z0-9\.\-_]*[A-Za-z0-9]*@([A-Za-z0-9]+([A-Za-z0-9-]*[A-Za-z0-9]+)*\.)+[A-Za-z]*$/gi,
+    errorMessage: 'Ошибка'
+  },
+  {
     rule: 'required',
     errorMessage: 'Введите электронную почту'
   },
